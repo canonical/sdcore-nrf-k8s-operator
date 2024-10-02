6462878bc480f934e773b7d2be33481751da86e5
Ghislain Bourgeois <ghislain.bourgeois@canonical.com>
2024-10-02 14:58:30 -0400

    chore: Refactor the Terraform module to follow spec CC006

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,21 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-resource "juju_application" "sdcore-nrf-k8s" {
+resource "juju_application" "nrf" {
   name  = var.app_name
-  model = var.model_name
+  model = var.model
 
   charm {
-    name    = "sdcore-nrf-k8s"
-    channel = var.channel
+    name     = "sdcore-nrf-k8s"
+    channel  = var.channel
+    revision = var.revision
   }
-  units = 1
-  trust = true
+
+  config      = var.config
+  constraints = var.constraints
+  units       = var.units
+  resources   = var.resources
+  trust       = true
 }
 
 
