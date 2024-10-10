7a8059e845f4e90afea1140a46e47aa078e80929
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-10-10 15:58:54 -0400

    chore: use built-in testing from ops
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- tests/unit/charms/sdcore_nrf/v0/test_fiveg_nrf_requirer_interface.py
+++ tests/unit/charms/sdcore_nrf/v0/test_fiveg_nrf_requirer_interface.py
@@ -4,7 +4,7 @@
 from unittest.mock import patch
 
 import pytest
-import scenario
+from ops import testing
 from ops.charm import ActionEvent, CharmBase
 
 from lib.charms.sdcore_nrf_k8s.v0.fiveg_nrf import NRFAvailableEvent, NRFBrokenEvent, NRFRequires
@@ -34,7 +34,7 @@ class TestFiveGNRFRequirer:
 
     @pytest.fixture(autouse=True)
     def context(self):
-        self.ctx = scenario.Context(
+        self.ctx = testing.Context(
             charm_type=DummyFiveGNRFRequirerCharm,
             meta={
                 "name": "nrf-requirer-charm",
@@ -48,14 +48,14 @@ class TestFiveGNRFRequirer:
     def test_given_nrf_information_in_relation_data_when_relation_changed_then_nrf_available_event_emitted(  # noqa: E501
         self,
     ):
-        nrf_relation = scenario.Relation(
+        nrf_relation = testing.Relation(
             endpoint="fiveg_nrf",
             interface="fiveg_nrf",
             remote_app_data={
                 "url": "http://nrf.com",
             },
         )
-        state_in = scenario.State(
+        state_in = testing.State(
             relations=[nrf_relation],
         )
 
@@ -68,11 +68,11 @@ class TestFiveGNRFRequirer:
     def test_given_nrf_information_not_in_relation_data_when_relation_changed_then_nrf_available_event_not_emitted(  # noqa: E501
         self,
     ):
-        nrf_relation = scenario.Relation(
+        nrf_relation = testing.Relation(
             endpoint="fiveg_nrf",
             interface="fiveg_nrf",
         )
-        state_in = scenario.State(
+        state_in = testing.State(
             relations=[nrf_relation],
         )
 
@@ -83,14 +83,14 @@ class TestFiveGNRFRequirer:
     def test_given_invalid_nrf_information_in_relation_data_when_relation_changed_then_nrf_available_event_not_emitted(  # noqa: E501
         self,
     ):
-        nrf_relation = scenario.Relation(
+        nrf_relation = testing.Relation(
             endpoint="fiveg_nrf",
             interface="fiveg_nrf",
             remote_app_data={
                 "pizza": "steak",
             },
         )
-        state_in = scenario.State(
+        state_in = testing.State(
             relations=[nrf_relation],
         )
 
@@ -101,14 +101,14 @@ class TestFiveGNRFRequirer:
     def test_given_invalid_nrf_information_in_relation_data_when_relation_changed_then_error_is_logged(  # noqa: E501
         self, caplog
     ):
-        nrf_relation = scenario.Relation(
+        nrf_relation = testing.Relation(
             endpoint="fiveg_nrf",
             interface="fiveg_nrf",
             remote_app_data={
                 "pizza": "steak",
             },
         )
-        state_in = scenario.State(
+        state_in = testing.State(
             relations=[nrf_relation],
         )
 
@@ -119,14 +119,14 @@ class TestFiveGNRFRequirer:
     def test_given_nrf_information_in_relation_data_when_get_nrf_url_is_called_then_expected_url_is_returned(  # noqa: E501
         self,
     ):
-        nrf_relation = scenario.Relation(
+        nrf_relation = testing.Relation(
             endpoint="fiveg_nrf",
             interface="fiveg_nrf",
             remote_app_data={
                 "url": "http://nrf.com",
             },
         )
-        state_in = scenario.State(
+        state_in = testing.State(
             relations=[nrf_relation],
         )
 
@@ -140,11 +140,11 @@ class TestFiveGNRFRequirer:
     def test_given_nrf_information_not_in_relation_data_when_get_nrf_url_then_returns_none(  # noqa: E501
         self, caplog
     ):
-        nrf_relation = scenario.Relation(
+        nrf_relation = testing.Relation(
             endpoint="fiveg_nrf",
             interface="fiveg_nrf",
         )
-        state_in = scenario.State(
+        state_in = testing.State(
             relations=[nrf_relation],
         )
 
@@ -159,14 +159,14 @@ class TestFiveGNRFRequirer:
     def test_given_nrf_information_in_relation_data_is_not_valid_when_get_nrf_url_then_returns_none_and_error_is_logged(  # noqa: E501
         self, caplog
     ):
-        nrf_relation = scenario.Relation(
+        nrf_relation = testing.Relation(
             endpoint="fiveg_nrf",
             interface="fiveg_nrf",
             remote_app_data={
                 "pizza": "steak",
             },
         )
-        state_in = scenario.State(
+        state_in = testing.State(
             relations=[nrf_relation],
         )
 
@@ -179,11 +179,11 @@ class TestFiveGNRFRequirer:
         assert "Invalid relation data: {'pizza': 'steak'}" in caplog.messages
 
     def test_given_nrf_relation_created_when_relation_broken_then_nrf_broken_event_emitted(self):
-        nrf_relation = scenario.Relation(
+        nrf_relation = testing.Relation(
             endpoint="fiveg_nrf",
             interface="fiveg_nrf",
         )
-        state_in = scenario.State(
+        state_in = testing.State(
             relations=[nrf_relation],
         )
 
