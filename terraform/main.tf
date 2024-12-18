a347d6ff3276b20ec1046f6fb77c64e60ab12ef2
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2024-12-18 09:03:59 +0100

    Uses ubuntu@24.04 in TF module and itests

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,7 @@ resource "juju_application" "nrf" {
     name     = "sdcore-nrf-k8s"
     channel  = var.channel
     revision = var.revision
+    base     = var.base
   }
 
   config      = var.config
